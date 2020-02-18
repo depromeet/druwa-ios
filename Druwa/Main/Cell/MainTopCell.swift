7be1ae1790cef982dcbb9be1ee7d35528ad1a44b
양혜리 <leeyang6424@interpark.com>
2020-02-18 12:43:05 +0900

    [Feature] main top 그라데이션뷰 추가

--- Druwa/Main/Cell/MainTopCell.swift
+++ Druwa/Main/Cell/MainTopCell.swift
@@ -12,12 +12,18 @@ class MainTopCell: UITableViewCell {
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var gradationView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         contentView.backgroundColor = .gray400
         pageControl.pageIndicatorTintColor = .gray100
         pageControl.currentPageIndicatorTintColor = .main200
+               var colors = [UIColor]()
+        colors.append(.clear)
+        colors.append(UIColor(red: 1, green: 1, blue: 1, alpha: 0.5))
+         let gradientLayer = CAGradientLayer(frame: contentView.frame, colors: colors, startPoint: CGPoint(x: 0.5, y:0), endPoint: CGPoint(x:0.5, y:1))
+         gradationView.image = gradientLayer.createGradientImage()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
